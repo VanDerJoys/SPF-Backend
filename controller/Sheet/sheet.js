b24d581f9b111f5d696f76e21415775831e104e8
Aloedeveloper2 <82895671+Aloedeveloper2@users.noreply.github.com>
2021-08-17 12:27:39 +0100

    End of sheet management

--- controller/Sheet/sheet.js
+++ controller/Sheet/sheet.js
@@ -35,6 +35,40 @@ class Calls{
         }
     }
 
+    async getAllByPost(){
+        try {
+            let sheets = await SheetSchema.aggregate([
+                {
+                    $group: {
+                        _id: "$_id",
+                        totalCalls: { $sum: "$calls" },
+                        totalNotebooks: { $sum: "$notebooks"},
+                        totalArguments: { $sum: "$arguments"},
+                        totalOrders: { $sum: "$orders"},
+                        totalBusy_calls: { $sum: "$busy_calls" },
+                        totalUnavailables: { $sum: "$unavailable" },
+                        totalUnreachables: { $sum: "$unreachable" },
+                        totalDo_not_calls: { $sum: "$do_not_call" },
+                        post: {$push: "$post"}
+                    }
+                }
+            ])
+            let results = await SheetSchema.populate(sheets, 
+                {
+                    path: "post", 
+                    select: {
+                        __v: 0, 
+                        created_at: 0
+                    }
+                }
+            )
+            return results;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
     async getSheetOfOnePost(post_id){
         try {
             let sheets = await SheetSchema.aggregate([
@@ -53,12 +87,43 @@ class Calls{
                         totalBusy_calls: { $sum: "$busy_calls" },
                         totalUnavailables: { $sum: "$unavailable" },
                         totalUnreachables: { $sum: "$unreachable" },
-                        totalDo_not_calls: { $sum: "$do_not_call" }
+                        totalDo_not_calls: { $sum: "$do_not_call" },
+                        post: {$push: "$post"}
                     }
                 }
             ])
-            return sheets;
+            let results = await SheetSchema.populate(sheets, {path: "post", select: {__v: 0, created_at: 0}})
+            return results;
         } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async updateSheet(sheet_id, data){
+        try{
+            let sheet = await SheetSchema.updateOne({_id: sheet_id}, {
+                calls: data.call,
+                notebooks: data.notebook,
+                arguments: data.argument,
+                orders: data.order,
+                busy_calls: data.busy_call,
+                unavailable: data.unavailable,
+                unreachable: data.unreachable,
+                do_not_call: data.do_not_call
+            });
+            return sheet;
+        }catch(error){
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getAllSheetsOfOnePost(post_id){
+        try{
+            let sheets = await SheetSchema.find({post: post_id}, {__v: 0});
+            return sheets;
+        }catch(error){
             console.log(error);
             throw error;
         }
@@ -66,7 +131,7 @@ class Calls{
 
     async deleteOneSheet(sheet_id){
         try {
-            let call = await SheetSchema.deleteOne({_id: sheet_id}, {__v: 0});
+            let call = await SheetSchema.deleteOne({_id: sheet_id});
             return call;
         } catch (error) {
             console.log(error);
--- routes/Sheet/sheet.js
+++ routes/Sheet/sheet.js
@@ -21,6 +21,17 @@ router.post('/', (req, res)=>{
         console.log(error);
         res.status(400).send('Une erreur est survenue');
     })
+});
+
+// get all sheets by post
+router.get('/', (req, res)=>{
+    let sheet = new Sheet();
+    sheet.getAllByPost().then(response =>{
+        res.status(200).send(response);
+    }).catch(error =>{
+        console.log(error);
+        res.status(400).send('Une erreur est survenue');
+    })
 })
 
 // get a sheet of a single post
@@ -34,11 +45,38 @@ router.get('/:post_id', (req, res)=>{
     })
 })
 
+// get all sheets of a post
+router.get('/all/:post_id', (req, res)=>{
+    let sheet = new Sheet();
+    sheet.getAllSheetsOfOnePost(req.params.post_id).then(response =>{
+        res.status(200).send(response);
+    }).catch(error =>{
+        console.log(error);
+        res.status(400).send('Une erreur est survenue');   
+    });
+});
+
+// update a sheet
+router.put('/:sheet_id', (req, res)=>{
+    let sheet = new Sheet();
+    sheet.updateSheet(req.params.sheet_id, req.body).then(response =>{
+        if(Boolean(response.nModified)){
+            res.status(200).send(Boolean(response.nModified));
+        }
+        else{
+            res.status(404).send("La fiche n'existe pas")
+        }
+    }).catch(error =>{
+        console.log(error);
+        res.status(400).send("Une erreur est survenue");
+    });
+});
+
 // delete a single sheet
 router.delete('/:sheet_id', (req, res)=>{
     let sheet = new Sheet();
     sheet.deleteOneSheet(req.params.sheet_id).then(response =>{
-        res.status(200).send(response);
+        res.status(200).send(Boolean(response.deletedCount));
     }).catch(error =>{
         console.log(error);
         res.status(400).send('Une erreur est survenue');
