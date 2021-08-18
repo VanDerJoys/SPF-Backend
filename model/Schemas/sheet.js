cfed6aca851ffcf1513e617da5beba9d1c6562f7
aloedeveloper1 <82831518+aloedeveloper1@users.noreply.github.com>
2021-08-18 16:26:30 +0200

    Gestion des fiches

--- model/Schemas/sheet.js
+++ model/Schemas/sheet.js
@@ -4,52 +4,43 @@ const SheetSchema = new mongoose.Schema({
     post_id: {  // identifiant du poste
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: "Accounts"
+        ref: "Posts"
     },
     calls:{  // appelé
         type: Number,
         required: true,
-        default: 0
     },
     notebooks: {// rendez-vous
         type: Number,
         required: true,
-        default: 0
     },
     arguments: {  // argumentaires
         type: Number,
         required: true,
-        default: 0
     },
     orders: {  // commandes
         type: Number,
         required: true,
-        default: 0
     },
     busy_calls: {  // appel en absence
         type: Number,
         required: true,
-        default: 0
     },
     unavailable: {  // indisponible
         type: Number,
         required: true,
-        default: 0
     },
     unreachable: {  // injoignable
         type: Number,
         required: true,
-        default: 0
     },
     do_not_call: {  // ne plus appeler
         type: Number,
         required: true,
-        default: 0
     },
     tranche: {  // tranche horaire
         type: String,
         required: true,
-        default: 0
     },
     created_at:{
         type: Date,
