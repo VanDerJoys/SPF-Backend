1edab7534e420cf1fdfcafd9901841cdcece9474
Aloedeveloper2 <82895671+Aloedeveloper2@users.noreply.github.com>
2021-10-26 10:52:08 +0100

    Managing dashboard per period

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -1,7 +1,45 @@
 const Sheet = require('../model/Schemas/sheet');
 const dataParser = require('../helpers/dashboard-data-parser');
 
+const moment = require('moment');
+
 class DashboardController{
+
+    async getPeriodicData(period){
+        try {
+            let sheets = await Sheet.aggregate([
+                {
+                    $match: {
+                        created_at: period
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$post",
+                        calls: {
+                            $sum: {
+                                $add: [ "$rdv", "$argument", "$busy_call", "$unavailable", "$unreachable", "$order", "$doNotCall" ]
+                            }
+                        },
+                        post: { $first: "$post" },
+                    },
+                },
+            ]);
+            let results = await Sheet.populate(sheets, {
+                path: "post",
+                select: {
+                    __v: 0,
+                    created_at: 0,
+                    available: 0,
+                    account: 0
+                },
+            });
+            return dataParser(results);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
 // get Events
     async getDashboardData() {
         try {
@@ -27,12 +65,31 @@ class DashboardController{
                     account: 0
                 },
             });
-            // return results;
             return dataParser(results);
         } catch (error) {
         console.log(error);
         throw error;
         }
+    }
+
+    async getToday(){
+        let results = await this.getPeriodicData(moment().format('YYYY-MM-DD'));
+        return results;
+    }
+
+    async getYesterday(){
+        let results = await this.getPeriodicData(moment().subtract(1, 'days').format('YYYY-MM-DD'));
+        return results;
+    }
+
+    async getSevenDays(){
+        let results = await this.getPeriodicData(moment().subtract(7, 'days').format('YYYY-MM-DD'));
+        return results;
+    }
+
+    async getThirtyDays(){
+        let results = await this.getPeriodicData(moment().subtract(30, 'days').format('YYYY-MM-DD'));
+        return results;
     }
 }
 
