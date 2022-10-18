1ccb1772705b4560c127ecbb48698264e08fe421
VanDerJoys <52749354+VanDerJoys@users.noreply.github.com>
2022-10-18 16:13:36 +0100

    Modification of audio upload system

--- controller/listening.js
+++ controller/listening.js
@@ -1,21 +1,8 @@
 const Listening = require("../model/Schemas/Listening");
 
 class ListeningController{
-    constructor(observation, finalNote, comment6, note6, comment5, note5, comment4, note4, comment3, note3, comment2, note2, comment1, note1, post){
+    constructor(observation, post){
         this.post = post;
-        this.finalNote = finalNote;
-        this.comment5 = comment6;
-        this.note5 = note6;
-        this.comment5 = comment5;
-        this.note5 = note5;
-        this.comment4 = comment4;
-        this.note4 = note4;
-        this.comment3 = comment3;
-        this.note3 = note3;
-        this.comment2 = comment2;
-        this.note2 = note2;
-        this.comment1 = comment1;
-        this.note1 = note1;
         this.observation = observation;
     }
 
@@ -23,19 +10,6 @@ class ListeningController{
         try{
             const listening = new Listening({
                 post: this.post,
-                finalNote: this.finalNote,
-                comment6: this.comment6,
-                note6: this.note6,
-                comment5: this.comment5,
-                note5: this.note5,
-                comment4: this.comment4,
-                note4: this.note4,
-                comment3: this.comment3,
-                note3: this.note3,
-                comment2: this.comment2,
-                note2: this.note2,
-                comment1: this.comment1,
-                note1: this.note1,
                 observation: this.observation
             });
             const results = await listening.save();
@@ -58,11 +32,14 @@ class ListeningController{
 
     async deleteListening(id){
         try{
-            const listening = await Listening.deleteOne({_id: id});
-            return listening;
+            await Listening.deleteOne({_id: id});
+            return {
+                status: 200,
+                message: "Ecoute supprimée"
+            }
         }catch(error){
             console.log(error);
-            throw error;
+            throw {status: 400, message: "Une erreur est survenue"}
         }
     }
 
--- routes/Listening/listening.js
+++ routes/Listening/listening.js
@@ -2,6 +2,7 @@ const express = require('express');
 const formidable = require('formidable');
 const path = require('path');
 const ListeningController = require('../../controller/listening');
+const ListeningModel = require("../../model/Schemas/Listening");
 // const { verifyToken } = require('../../helpers/web-token');
 
 const router = express.Router();
@@ -10,19 +11,6 @@ const router = express.Router();
 router.post('/', (req, res)=>{
     const listening = new ListeningController(
         req.body.data.observation,
-        req.body.data.finalNote,
-        req.body.data.comment6,
-        req.body.data.note6,
-        req.body.data.comment5,
-        req.body.data.note5,
-        req.body.data.comment4,
-        req.body.data.note4,
-        req.body.data.comment3,
-        req.body.data.note3,
-        req.body.data.comment2,
-        req.body.data.note2,
-        req.body.data.comment1,
-        req.body.data.note1,
         req.body.data.post,
     );
     listening.createListening().then(response =>{
@@ -45,34 +33,41 @@ router.get('/', (req, res)=>{
 
 // File upload
 router.post('/:listeningId/file', (req, res)=>{
-    const listening = new ListeningController();
-    const form = formidable();
-    form.parse(req, (err, fields, files)=>{
-        if(err){
-            console.log(err)
-            res.status(400).send('Le fichier est endommagé!');
+    let form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+		if (err) {
+			console.log(err);
+			res.status(400).send("Erreur de chargement des fichiers");
+		}else{
+            await ListeningModel.updateOne({_id: req.params.listeningId}, {filePath: `${files.audio.newFilename}_${files.audio.originalFilename}`});
+            res.send("Fichiers reçus").status(200);
         }
-    });
+	});
 
-    form.on('fileBegin', (name, file)=>{
-        file.filepath = path.join('C:/Apache/htdocs/audio/', file.originalFilename);
-        listening.upload(req.params.listeningId, `http://localhost/audio/${file.originalFilename}`).then(response =>{
-            res.status(201).send(response);
-        }).catch(error =>{
-            console.log(error);
-            res.status(500).send('Une erreur est survenue');
-        })
-    })
+	form.on("fileBegin", (name, file) => {
+		file.filepath = path.join(
+			`${__dirname}/../../uploads`,
+			`${file.newFilename}_${file.originalFilename}`
+		);
+	});
 })
+
+router.get("/audio/:filename", (req, res) => {
+	var options = {
+		root: path.join(__dirname + "/../../uploads"),
+	};
+
+	res.sendFile(req.params.filename, options);
+});
 
 // delete a listening
 router.delete('/:id', (req, res)=>{
     const listening = new ListeningController();
     listening.deleteListening(req.params.id).then(response =>{
-        res.status(200).send(Boolean(response.deletedCount));
+        res.status(response.status).send(response.message);
     }).catch(error =>{
         console.log(error);
-        res.status(400).send('Une erreur est survenue');
+        res.status(error.status).send(error.message);
     });
 });
 
